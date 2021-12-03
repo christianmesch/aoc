a1f788977bc51ef259e4ae57186d0641de04c929
Christian Mesch <christian.mesch@sigmatechnology.se>
2021-12-03 15:32:44 +0100

    Make 2021 day 3 shorter, because why not?

--- 2021/03.js
+++ 2021/03.js
@@ -4,10 +4,8 @@ const input = require('./utils')
   .read(args[2]).map((val) => val.split('').map((v) => parseInt(v, 10)));
 
 const frequency = (list, mostCommon = true) => {
-  const freq = list.reduce((acc, curr) => {
-    curr.forEach((val, i) => { acc[i] += val; });
-    return acc;
-  }, new Array(list[0].length).fill(0));
+  const freq = list.reduce((acc, curr) =>
+    acc.map((val, i) => val + curr[i]), new Array(list[0].length).fill(0));
 
   return mostCommon ? freq.map((f) => f >= (list.length / 2) ? 1 : 0) : freq.map((f) => f < (list.length / 2) ? 1 : 0);
 };
@@ -19,24 +17,18 @@ const part1 = (input) => {
   return parseInt(gamma.join(''), 2) * parseInt(epsilon.join(''), 2);
 };
 
-const part2 = (input) => {
-  let oxyList = input;
-  for (let i = 0; i < oxyList[0].length && oxyList.length > 1; i++) {
-    const freq = frequency(oxyList);
-    oxyList = oxyList.filter((val) => val[i] === freq[i]);
+const part2 = (input, mostCommon = true) => {
+  let list = input;
+  for (let i = 0; i < list[0].length && list.length > 1; i++) {
+    const freq = frequency(list, mostCommon);
+    list = list.filter((val) => val[i] === freq[i]);
   }
 
-  let coList = input;
-  for (let i = 0; i < coList[0].length && coList.length > 1; i++) {
-    const freq = frequency(coList, false);
-    coList = coList.filter((val) => val[i] === freq[i]);
-  }
-
-  return parseInt(oxyList[0].join(''), 2) * parseInt(coList[0].join(''), 2);
+  return parseInt(list[0].join(''), 2);
 };
 
 if (!args[3] || args[3] === '1') {
   console.log(part1(input));
 } else {
-  console.log(part2(input));
+  console.log(part2(input) * part2(input, false));
 }
