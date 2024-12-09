e83e29f42026e65af6583c267bba90f797f6a603
Christian Mesch <christianmesch@gmail.com>
2024-12-09 16:41:57 +0100

    2024 day 09

---
+++ 2024/09.js
@@ -0,0 +1,61 @@
+const args = process.argv;
+const { inputs } = require('./utils');
+
+const input = inputs
+    .read(args[2])[0]
+    .split('')
+        .map(Number)
+        .map((v, i) => ({
+            original: v,
+            roomLeft: i % 2 ? v : 0,
+            vals: i % 2 ? [] : new Array(v).fill(i / 2)
+        }));
+
+const part1 = () => {
+    let r = input.length - 1;
+    let right = input[r];
+    for (let l = 1; l < r; l += 2) {
+        const left = input[l];
+        while (left.roomLeft) {
+            if (!right.vals.length) {
+                r -= 2;
+                right = input[r];
+
+                if (r < l) break;
+            }
+
+            left.vals.push(right.vals.pop());
+            left.roomLeft--;
+        }
+    }
+
+    return input.flatMap((v) => v.vals)
+        .reduce((acc, curr, i) => acc + i * curr, 0);
+};
+
+const part2 = () => {
+    for (let r = input.length - 1; r > 0; r -= 2) {
+        const right = input[r];
+        for (let l = 0; l < r; l++) {
+            const left = input[l];
+            if (left.roomLeft >= right.original) {
+                left.vals.push(...right.vals);
+                right.vals = [];
+                right.roomLeft = right.original;
+                left.roomLeft -= right.original;
+                break;
+            }
+        }
+    }
+
+    return input.flatMap((v) => {
+        if (v.roomLeft) v.vals.push(...new Array(v.roomLeft).fill('.'));
+        return v.vals;
+    }).reduce((acc, curr, i) => curr !== '.' ? acc + i * curr : acc, 0);
+};
+
+if (!args[3] || args[3] === '1') {
+    console.log(part1());
+} else {
+    console.log(part2());
+}
--- 2024/run.sh
+++ 2024/run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-DAYS=( 01 02 03 04 05 06 07 08 )
+DAYS=( 01 02 03 04 05 06 07 08 09 )
 
 # Wall of shame (VERY slow solutions):
 
